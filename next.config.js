7e48f5aa8aa447af30a66a601b858201d508a294
triveni1726 <shivani.kaniya@triveniglobalsoft.com>
2023-10-06 11:04:05 +0530

    first change

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
     const nextConfig = {
         images:{
             domains:['i.dummyjson.com','api.slingacademy.com'],
-        }
+        },
+        env: {
+            SERVER: process.env.SERVER,
+          },
     }
 module.exports=nextConfig
